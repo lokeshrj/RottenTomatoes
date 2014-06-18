cb886db56af5f810239f50afd9affd1c14ae5026
Lokesh <ljagasia@salesforce.com>
2014-06-18 01:17:48 -0700

    Initial Commit

---
+++ RottenTomatoes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RottenTomatoes
+//
+//  Created by Lokesh Jagasia on 6/18/14.
+//  Copyright (c) 2014 ljagasia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
